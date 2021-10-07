214f29c9d2a8066256e7ac7c03684e1cc6227bcf
Max K <mkoerner570@gmail.com>
2021-10-07 15:49:57 -0400

    user signup route created

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth");
@@ -6,34 +8,41 @@ const { User } = require("../../db/models");
 const { restoreUser } = require("../../utils/auth")
 const { requireAuth } = require("../../utils/auth")
 
-//GET /set-token-cookie test
-router.get('/set-token-cookie', asyncHandler(async (req,res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        },
-    })
-    setTokenCookie(res,user);
-    return res.json({user})
-}))
+router.use('/session',sessionRouter);
+router.use('/users', usersRouter);
 
-//GET /api/restore-user test
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req,res) => {
-        return res.json(req.user)
-    }
-);
+router.post('/test',(req,res) => {
+    res.json({requestBody:req.body});
+})
 
-//GET /api/require-auth test
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req,res) => {
-        return res.json(req,user)
-    }
-)
+// //GET /set-token-cookie test
+// router.get('/set-token-cookie', asyncHandler(async (req,res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         },
+//     })
+//     setTokenCookie(res,user);
+//     return res.json({user})
+// }))
+
+// //GET /api/restore-user test
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req,res) => {
+//         return res.json(req.user)
+//     }
+// );
+
+// //GET /api/require-auth test
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req,res) => {
+//         return res.json(req,user)
+//     }
+// )
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
