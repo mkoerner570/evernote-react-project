7f8115786cdd332e4a694078ec95b3ace92c2b8c
Max K <mkoerner570@gmail.com>
2021-10-07 13:46:47 -0400

    user auth routes tested successfully, phase 3 done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,40 @@
 const router = require('express').Router();
 
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../../utils/auth");
+const { User } = require("../../db/models");
+const { restoreUser } = require("../../utils/auth")
+const { requireAuth } = require("../../utils/auth")
+
+//GET /set-token-cookie test
+router.get('/set-token-cookie', asyncHandler(async (req,res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        },
+    })
+    setTokenCookie(res,user);
+    return res.json({user})
+}))
+
+//GET /api/restore-user test
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req,res) => {
+        return res.json(req.user)
+    }
+);
+
+//GET /api/require-auth test
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req,res) => {
+        return res.json(req,user)
+    }
+)
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
