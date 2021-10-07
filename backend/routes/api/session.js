c7f467ad17ac002ee441a5257abf7e81496d9564
Max K <mkoerner570@gmail.com>
2021-10-07 16:16:35 -0400

    validate login created and tested

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require("../../utils/auth")
-const { User } = require("../../db/models")
+const { User } = require("../../db/models");
+const {check} = require("express-validator");
+const {handleValidationErrors} = require('../../utils/validation')
 const router = express.Router();
 
-//login route
+//Login Validation middleware
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage("Please provide a valid email or username"),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide a password"),
+    handleValidationErrors,
+];
+
+//Login route
 router.post(
     '/',
-    asyncHandler(async(req,res,next) => {
-        const {credential,password} = req.body;
-        const user = await User.login({credential,password});
+    validateLogin,
+    asyncHandler(async (req, res, next) => {
+      const { credential, password } = req.body;
 
-        if(!user){
-            const err = new Error('Login failed');
-            err.status = 401;
-            err.title = 'Login Failed';
-            err.errors = ['The provided credentials were invalid'];
-            return next(err);
-        }
+      const user = await User.login({ credential, password });
 
-        await setTokenCookie(res,user);
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
 
-        return res.json({
-            user,
-        });
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user,
+      });
     }),
 );
 
-//logout route
+//Logout route
 router.delete(
     '/',
     (_req,res) => {
