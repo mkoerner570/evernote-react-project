710f6dd3cd2135c9ce523554533ebb220f71ee53
Max K <mkoerner570@gmail.com>
2021-10-11 18:51:54 -0400

    getting note details started

--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -10,33 +10,96 @@ const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
 router.get(
-	'/',
-	csrfProtection,
-	asyncHandler(async (req,res,next) => {
-		return res.json({
-			user,
-		})
+	'/:id(\\d+)',
+	asyncHandler(async (req, res, next) => {
+		const noteId = req.params.id;
+
+		const note = await db.Notes.findByPk(noteId, { include: { model: db.Notebooks } });
+
+		let notebooks;
+
+		let userId;
+		if (req.user.id) {
+			userId = req.user.id;
+
+			notebooks = await db.Notebooks.findAll({
+				where: {
+					userId,
+				},
+				include: db.Note,
+			});
+		} else {
+			notebooks = [];
+		}
+
+	// 	let inToolbox = false;
+	// 	let toolboxId;
+	// 	let toolboxName;
+
+	// 	for (const box of toolboxes) {
+	// 		if (!inToolbox) {
+	// 			const apis = box.Apis;
+	// 			for (const api of apis) {
+	// 				if (!inToolbox) {
+	// 					if (api.id === Number(api_id)) {
+	// 						inToolbox = true;
+	// 						toolboxId = box.id;
+	// 						toolboxName = box.name;
+	// 						break;
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+
+	// 	const toolboxInfo = { inToolbox, toolboxId, toolboxName };
+
+	// 	const reviews = await db.Review.findAll({
+	// 		where: {
+	// 			api_id,
+	// 		},
+	// 		order: [['updatedAt', 'DESC']],
+	// 		include: { model: db.User },
+	// 	});
+
+	// 	const avgNumber = await reviewAvgRating(api_id);
+
+	// 	if (api) {
+	// 		return res.render('api', {
+	// 			title: `Ace API - ${api.name}`,
+	// 			api,
+	// 			toolboxes,
+	// 			avgRating: avgNumber,
+	// 			reviews,
+	// 			user_id,
+	// 			toolboxInfo,
+	// 		});
+	// 	} else {
+	// 		next();
+	// 	}
 	})
-)
+);
 
 router.post(
 	'/',
 	csrfProtection,
 	requireAuth,
 	asyncHandler(async (req, res, next) => {
-		const { userId, title, contents } = req.body;
+		const { title, contents } = req.body;
+		const userId = req.user.id;
+		console.log(userId)
 
 		const validatorErrors = validationResult(req);
 		if (validatorErrors.isEmpty()) {
 
 			const newNote = await db.Notes.create({
-				userId,
+				userId: userId,
                 // noteBooksId,
                 title,
                 contents
 			});
 			return res.json({
-				user,
+				newNote,
 			});
         }
 	})
